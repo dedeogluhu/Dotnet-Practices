ff70e4e2a75bfa8cdc12bb2924abca8d14c05e2a
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-07-07 14:26:31 +0300

    Started Expanding by Adding Teams

---
+++ OOPPrototype_Basketball/Classes/Team.cs
@@ -0,0 +1,18 @@
+﻿using OOPPrototype_Basketball.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPrototype_Basketball.Classes
+{
+    class Team
+    {
+        public string Name { get; set; }
+        public string Stadium { get; set; }
+        public List<Player> Players { get; set; }
+        public Conference WhichConference { get; set; }
+
+    }
+}
---
+++ OOPPrototype_Basketball/Enums/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPrototype_Basketball.Enums
+{
+    public enum Conference
+    {
+        Ease,
+        West
+    }
+}
