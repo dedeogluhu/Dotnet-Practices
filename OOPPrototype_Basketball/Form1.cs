4a2cae9581882b95e4c80edc07432d6fb0ab0364
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-07-07 13:23:56 +0300

    Started Creating Form app

--- OOPPrototype_Basketball/Form1.cs
+++ OOPPrototype_Basketball/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OOPPrototype_Basketball.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,25 @@ namespace OOPPrototype_Basketball
 {
     public partial class Form1 : Form
     {
+        PointGuard _stephenCurry;
+        ShootingGuard _klayThompshon;
+        SmallForward _andrewWiggins;
+        PowerForward _draymondGreen;
+        Center _willieCauleyStein;
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _stephenCurry = new PointGuard { Name = "Stephen Curry", Age = 32 };
+            _klayThompshon = new ShootingGuard { Name = "Klay Thompson", Age = 30 };
+            _andrewWiggins = new SmallForward { Name = "Andrew Wiggins", Age = 25 };
+            _draymondGreen = new PowerForward { Name = "Draymond Green", Age = 30 };
+            _willieCauleyStein = new Center { Name = "Willie Cauley Stein", Age = 26 };
         }
     }
 }
