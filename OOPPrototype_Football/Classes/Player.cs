6c1e1a39a057b0cb8ac44a6b093a4f9ed21bead1
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-07-09 14:08:41 +0300

    Created Interfaces

---
+++ OOPPrototype_Football/Classes/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPrototype_Football.Classes
+{
+    public abstract class Player
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public abstract string Shoot();
+        public abstract string Pass();
+    }
+}
---
+++ OOPPrototype_Football/Classes/Team.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPrototype_Football.Classes
+{
+    public class Team
+    {
+        public string Name { get; set; }
+        public string Stadium { get; set; }
+        public List<Player> Players { get; set; }
+        public string Play(Team team)
+        {
+            return $"Playing a Game with {team}";
+        }
+    }
+}
