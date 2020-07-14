8d5924aebaaff8dc294dc997e129fb9bf1b87a86
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-07-14 15:12:07 +0300

    Started a Restaurant Project

---
+++ OOPPrototype_Restaurant/Classes/Food.cs
@@ -0,0 +1,13 @@
+﻿namespace OOPPrototype_Restaurant.Classes
+{
+    public class Food
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Price}";
+        }
+    }
+}
---
+++ OOPPrototype_Restaurant/Classes/Table.cs
@@ -0,0 +1,13 @@
+﻿namespace OOPPrototype_Restaurant.Classes
+{
+    public class Table
+    {
+        public int TableNo { get; set; }
+        public int Fee { get; set; }
+
+        public void Payment()
+        {
+            Fee = 0;
+        }
+    }
+}
